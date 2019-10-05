dff13dfb0e7a78e3e38fcd65de7e3ae74cfb7bf2
JarrodLos <56139570+JarrodLos@users.noreply.github.com>
2019-10-05 16:25:44 -0600

    Where to put color

--- app/src/main/java/com/example/lab4exercise/Shape.java
+++ app/src/main/java/com/example/lab4exercise/Shape.java
@@ -4,5 +4,13 @@ package com.example.lab4exercise;
 public abstract class Shape {
     private int x;
     private int y;
+
+    // Add your colors:
+
+
+
+
+
+    // Last step
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
